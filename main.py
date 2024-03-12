56e5abe908ee2c2e22012d8e93068c1a9614c062
Palenhame <111377258+Palenhame@users.noreply.github.com>
2024-03-12 16:47:08 +0300

    dgf

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 def snail(snail_map):
-    pass
+    if snail_map[0]:
+        for i in snail_map:
+            if len(i) >= 3:
+                pass
+            else:
+                return i
+
+        answer = []
+        first_line, other = snail_map[0], *snail_map
+        answer.extend(first_line)
+        print(other)
+        return answer
+    return []
+
+
+print(snail([[1, 2, 3],
+             [4, 5, 6],
+             [7, 8, 9]]))
